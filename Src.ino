fb54734602171ee6912df72f5b1d7bd32dd338ed
Some Words <aretexting@gmail.com>
2025-03-13 14:27:41 +0200

    feat: add serial monitor commands

--- Src.ino
+++ Src.ino
@@ -1,26 +1,37 @@
 #include "RoboLeg.h"
 #include "Point.h"
 
-void parseSerialInput() {
-  if (Serial.available() > 0) {
-    String input = Serial.readStringUntil('\n');
-    input.trim();
-
-    if (input.equals("schema")) {
-      Serial.println(RoboLeg::getSchema());
-    } else {
-    }
-  }
-}
-
-Options options(9, 10, 11, 5, 3, 4, 6, Point(10, 0, 0));
+Options options(8, 9, 10, 5, 3, 4, 6, Point(10, 0, 0));
+RoboLeg* roboleg;
 
 void setup() {
   Serial.begin(9600);
+  roboleg = new RoboLeg(options);
 }
 
 void loop() {
-  parseSerialInput();
+  parseSerialCommand();
+}
 
-  RoboLeg roboleg(options);
+void parseSerialCommand() {
+  if (Serial.available() > 0) {
+    String input = Serial.readStringUntil('\n');
+    input.trim();
+    executeCommand(input);
+  }
+}
+
+void executeCommand(String command) {
+  if (command.startsWith("s ")) {
+    int spaceIndex = command.indexOf(' ', 2);
+    if (spaceIndex != -1) {
+      int servoNumber = command.substring(2, spaceIndex).toInt();
+      int angle = command.substring(spaceIndex + 1).toInt();
+      roboleg->moveServo(servoNumber, angle);
+    }
+  } else if (command == "scheme") {
+    Serial.println(RoboLeg::getSchema());
+  } else {
+    Serial.println("Unknown command");
+  }
 }
